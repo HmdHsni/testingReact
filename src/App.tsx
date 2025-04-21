bf0aefd79142dd8dcbdfc183a165f056c7326ccd
Hamideh Hasani <14024494@entekhab.local>
2025-04-21 16:07:41 +0330

    fix:add dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   console.log("testing GitHub Actions...");
+  console.log("kjhgjgjd")
   return (
     <div className="App">
       <header className="App-header">
