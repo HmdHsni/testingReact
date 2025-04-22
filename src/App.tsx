0c09790130247ca094930eebd38540b5f77f8946
Hamideh Hasani <14024494@entekhab.local>
2025-04-22 13:02:23 +0330

    fix:test rules

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+console.log("haminjooriiiiiiiiiiiiiii")
   return (
     <div className="App">
       <header className="App-header">
