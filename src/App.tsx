1fb3ed71e57918d083048ec418a676c4bb96a6a3
Hamideh Hasani <14024494@entekhab.local>
2025-04-22 11:23:27 +0330

    fix:remove console.log

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log("testing GitHub Actions...");
-  console.log("kjhgjgjd")
-  console.log("flow")
   return (
     <div className="App">
       <header className="App-header">
